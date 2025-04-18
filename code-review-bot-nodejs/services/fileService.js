139465df87f36f92a4f9fdf873ba735b4bb83ba4
ississippi <ississippi@outlook.com>
2025-04-18 10:37:34 -0400

    shell python project

--- code-review-bot-nodejs/services/fileService.js
+++ code-review-bot-nodejs/services/fileService.js
@@ -1,16 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 
-const filePath = path.join(__dirname, '..\\test-data\\python-snippets.json');
+const filePath = path.join(__dirname, '..\\..\\test-data\\python-snippets.json');
 jsonData = {};
-function getCodeSnippet() {
+function getRandomCodeSnippetFromFile(snippet_index_item = -1) {
     // "snippets": [
     //    "id"
     //    "snippet"
     //    "language"
     //    "repo_file_name"
     // ]  
+    console.log(`snippet_index_item: ${snippet_index_item}`);
+    const max_snippet_count = 200;
     return new Promise((resolve, reject) => {
+        if (snippet_index_item > max_snippet_count) {
+            snippet_index_item = -1;
+        }
+        if (snippet_index_item < 0) {
+            random_index = Math.floor(Math.random() * max_snippet_count - 1);
+        }
+        else{
+            random_index = snippet_index_item;
+        }
         fs.readFile(filePath, 'utf8', (err, data) => {
             if (err) {
                 console.error('Error reading JSON file:', err);
@@ -18,10 +29,9 @@ function getCodeSnippet() {
                 return;
             }
             try {
-                console.log('Parse JSON data.');
                 const jsonData = JSON.parse(data);
-                console.log('JSON data parsed.');
-                const resultData = jsonData.snippets[6].snippet;
+                console.log(`Snippet Index: ${random_index}`);
+                const resultData = jsonData.snippets[random_index].snippet;
                 resolve(resultData);
             } catch (parseErr) {
                 console.error('Error parsing JSON:', parseErr);
@@ -43,7 +53,7 @@ function getCodeSnippet() {
       
 }
 
-getCodeSnippet()
+getRandomCodeSnippetFromFile()
     .then((resultData) => {
         console.log('Snippet:', resultData);
     })
